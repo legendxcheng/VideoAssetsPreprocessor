c07f85ba2a9c3df2f70d23e9627b9399e316b6a8
Legendxcheng <legendxcheng@gmail.com>
2024-04-21 18:27:08 +0800

    支持多种格式

--- TaskProcessor.py
+++ TaskProcessor.py
@@ -7,10 +7,58 @@ import ffmpeg
 from loguru import logger
 import sqlite3
 from scenedetect import open_video, SceneManager, split_video_ffmpeg
+from moviepy.editor import VideoFileClip
 from scenedetect.detectors import AdaptiveDetector
 from scenedetect.video_splitter import split_video_ffmpeg
 from scenedetect.frame_timecode import FrameTimecode
 import subprocess
+import shutil
+import cv2
+import subprocess
+import json
+
+
+
+
+
+def get_video_orientation(video_path):
+    # 使用 OpenCV 读取视频宽度和高度
+    cap = cv2.VideoCapture(video_path)
+    if not cap.isOpened():
+        print("Error opening video file.")
+        return None
+
+    width = cap.get(cv2.CAP_PROP_FRAME_WIDTH)
+    height = cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
+    cap.release()
+
+    # 使用 ffprobe 获取旋转信息
+    cmd = [
+        'ffprobe', '-v', 'error', '-select_streams', 'v:0',
+        '-show_entries', 'stream_tags=rotate', '-of', 'json', video_path
+    ]
+    result = subprocess.run(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    try:
+        rotate = json.loads(result.stdout)['streams'][0]['tags']['rotate']
+    except (KeyError, IndexError):
+        rotate = "0"
+
+    # 判断视频实际显示方向
+    rotate = int(rotate)
+    if rotate in (90, 270):  # 视频被旋转了90度或270度
+        # 交换宽度和高度
+        width, height = height, width
+        
+    return width, height
+
+def get_video_properties(video_path):
+    '''
+    获取视频文件的持续时间、宽度和高度。
+    '''
+    with VideoFileClip(video_path) as video:
+        duration = int(video.duration * 1000000)  # 转换为微秒
+    width, height = get_video_orientation(video_path)
+    return duration, width, height
 
 def split_video_into_scenes(video_path, output_dir , threshold=15.0):
     # Open our video, create a scene manager, and add a detector.
@@ -20,13 +68,19 @@ def split_video_into_scenes(video_path, output_dir , threshold=15.0):
         AdaptiveDetector(adaptive_threshold=2, min_scene_len= 30 ))
     scene_manager.detect_scenes(video, show_progress=True)
     scene_list = scene_manager.get_scene_list()
-    fps = scene_list[0][0].framerate
-    newScneList = []
-    for item in scene_list:
-        frameNum = item[0].get_frames() + 3
-        kk = FrameTimecode(timecode=frameNum, fps=fps)
-        newScneList.append((kk, item[1]))
-    split_video_ffmpeg(video_path, newScneList, output_dir = output_dir, show_progress=True)
+    if len(scene_list) > 0:
+        fps = scene_list[0][0].framerate
+        newScneList = []
+        for item in scene_list:
+            frameNum = item[0].get_frames() + 3
+            kk = FrameTimecode(timecode=frameNum, fps=fps)
+            newScneList.append((kk, item[1]))
+        split_video_ffmpeg(video_path, newScneList, output_dir = output_dir, show_progress=True)
+    else:
+        
+        shutil.copy(video_path, 
+                    os.path.join(output_dir, os.path.basename(video_path)[:-4]+"-Scene-001" + video_path[-4:]))
+        # split_video_ffmpeg(video_path, scene_list, output_dir = output_dir, show_progress=True)
     pass
 
 def extract_chinese(text):
@@ -75,9 +129,9 @@ class TaskProcessor:
         conn.commit()
         return conn
     
-    def findPatterMp4(self, targetDir, prefix):
+    def findPatterMp4(self, targetDir, prefix, suffix):
         ret = []
-        patternStr = rf"""{prefix}-Scene-\d+\.mp4"""
+        patternStr = rf"""{prefix}-Scene-\d+\{suffix}"""
         pattern  = re.compile(patternStr)
         for filename in os.listdir(targetDir):
             if pattern.match(filename):
@@ -96,11 +150,18 @@ class TaskProcessor:
             for root, dirs, files in os.walk(srcDir):
                 for file in files:
                     # 检查文件扩展名是否为.mp4
-                    tagInFileName = extract_chinese(file)
-                    if file.endswith('.mp4'):
+                    extraTags = extract_chinese(file)
+                    
+                    
+                    if file.endswith('.mp4') or file.endswith('.MP4') or file.endswith('.mov') or file.endswith('.MOV'):
                         try:
                             absPath  = os.path.join(root, file)
-                            tmpAbsPath= os.path.join(os.path.dirname(absPath),"tmp.mp4")
+                            vDra, width ,height= get_video_properties(absPath)
+                            if width > height:
+                                extraTags.append("横屏")
+                            else:
+                                extraTags.append("竖屏")
+                            tmpAbsPath= os.path.join(os.path.dirname(absPath), f"tmp.{file[-3:]}")
                             if removeAudio:
                                 # 构建 FFmpeg 命令
                                 cmdStr = f"ffmpeg -i \"{absPath}\" -c:v copy -an \"{tmpAbsPath}\""
@@ -117,14 +178,14 @@ class TaskProcessor:
                                 output_dir=outputDir)
                             # 分割完视频以后，检查每一个切割后的视频是否存在
                             ntags = tags+""
-                            for tagsinFileName in tagInFileName:
+                            for tagsinFileName in extraTags:
                                 if not ntags.endswith(","):
                                     ntags += ","
                                 ntags += f" {tagsinFileName}"
                             
                             
                             matchedFiles = self.findPatterMp4(outputDir, 
-                                                              os.path.basename(absPath)[:-4])
+                                                              os.path.basename(absPath)[:-4], os.path.basename(absPath)[-4:])
                             for filePath in matchedFiles:
                                 if os.path.exists(filePath):
                                     dbConn.cursor().execute(f"""INSERT INTO ASSET_TAGS (fileName, tags) VALUES 
